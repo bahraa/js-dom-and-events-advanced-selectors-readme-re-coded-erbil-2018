f27d3dbe8d4da33528582dfaa4635521fd41f2a1
bahraa <bbahra98@gmail.com>
2018-10-07 12:31:53 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 
+function paragraphSelector()
+{
+ return $("p");
+}
+function lastImageSelector()
+{
+  return $("img:last");
+}
+function ninjaBabySelector()
+{
+  return $("img#baby-ninja");
+}
+function divSelector()
+{
+  return $("div.pics");
+}
+function firstListItem()
+{
+  return $("ul#pic-list li:first-child");
+}
